92595a77a0c37a13902a9c93f4455a55d289a5ca
kcfigaro <kcfigaro@gmail.com>
2022-11-20 06:00:02 +0100

    added python code

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    # welcome messages
+    messages = ['Welcome cloudacode.com',
+                'Python Flask App',
+                ]
+    # return render_template('hello.html', messages=messages)
+    return messages
